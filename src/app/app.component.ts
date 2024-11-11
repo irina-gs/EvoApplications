47c9dace99b5ff7af966aad5bbd6a011eed31a45
irina-gs <guskovai2014@mail.ru>
2024-11-11 21:24:31 +0300

    1.7. Tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'EvoApplications';
+export class AppComponent implements OnInit {
+  constructor(
+    private meta: Meta,
+    private title: Title
+  ) { }
+
+  ngOnInit(): void {
+    this.meta.addTag({ property: 'og:desc', content: 'root_desc' })
+    this.title.setTitle('Desk Page')
+  }
 }
