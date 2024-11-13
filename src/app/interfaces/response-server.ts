c6f558b4fcb897911539b622896febfeec7c61d6
irina-gs <guskovai2014@mail.ru>
2024-11-13 22:00:35 +0300

    2.1. TypeScript

---
+++ src/app/interfaces/response-server.ts
@@ -0,0 +1,6 @@
+export interface ResponseServer {
+    userId: number
+    id: number
+    title: string
+    completed: boolean
+}
---
+++ src/app/services/response.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { ResponseServer } from '../interfaces/response-server';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResponseService {
+  responseData: ResponseServer[] = [
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "delectus aut autem",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "quis ut nam facilis et officia qui",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "fugiat veniam minus",
+      "completed": false
+    }
+  ]
+
+  constructor() { }
+}
