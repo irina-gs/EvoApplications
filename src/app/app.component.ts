062bd562fdba92ccaaf57df486faf7ee17accad4
irina-gs <guskovai2014@mail.ru>
2024-11-17 22:55:55 +0300

    2.3. RxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'EvoApplications';
+  counterArray: number[] = []
+  randomNumbersArray: string[] = []
+
+  counterSubscription$!: Subscription
+  randomNumbersSubscription$!: Subscription
+
+  isEnableStartButton: boolean = true
+  isEnableStopButton: boolean = false
+  isEnableStopButtonForCounter: boolean = false
+  isEnableStopButtonForRandom: boolean = false
+
+  startStreams() {
+    this.counterArray = []
+    this.randomNumbersArray = []
+
+    const interval$ = interval(2000)
+
+    this.counterSubscription$ = interval$.subscribe((value) => {
+      this.counterArray.push(value)
+    })
+    this.randomNumbersSubscription$ = interval$.pipe(
+      map(() => `Random Value: ${Math.floor(Math.random() * 1000)}`)
+    )
+    .subscribe((value) => {
+      this.randomNumbersArray.push(value)
+    })
+
+    this.isEnableStartButton = false
+    this.isEnableStopButton = true
+    this.isEnableStopButtonForCounter = true
+    this.isEnableStopButtonForRandom = true
+  }
+
+  stopCounterStream() {
+    this.counterSubscription$.unsubscribe()
+
+    this.isEnableStopButtonForCounter = false
+    if (!this.isEnableStopButtonForRandom) {
+      this.isEnableStartButton = true
+      this.isEnableStopButton = false
+    }
+  }
+
+  stopRandomNumbersStream() {
+    this.randomNumbersSubscription$.unsubscribe()
+
+    this.isEnableStopButtonForRandom = false
+    if (!this.isEnableStopButtonForCounter) {
+      this.isEnableStartButton = true
+      this.isEnableStopButton = false
+    }
+  }
+
+  stopStreams() {
+    this.stopCounterStream()
+    this.stopRandomNumbersStream()
+  }
 }
